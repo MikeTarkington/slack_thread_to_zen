9576c88c8f7b74960cc13338897861a931e10eb8
MikeTarkington <tarkmike@gmail.com>
2018-04-06 10:24:44 +0000

    Attempting to adjust routing to recieve post from slack by using a new post route as the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post 'message/slash_command' => 'message#slash_command'
 
-  root 'message/slash_command'
+  root 'message#slash_command'
 
   get 'message/slack_thread'
 
