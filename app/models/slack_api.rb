88303263002498369c8572f8551cfb045ba05d60
Mike Tarkington <tarkmike@gmail.com>
2018-04-08 01:49:52 -0700

    Fix initialization of SlackApi model for initial testing.

--- app/models/slack_api.rb
+++ app/models/slack_api.rb
@@ -2,6 +2,7 @@ class SlackApi < ApplicationRecord
 
   require 'uri'
   require 'net/http'
+  require 'json'
 
   def initialize
     url = URI("https://slack.com/api/search.messages?token=#{ENV['SLACK_TOKEN']}&query=everyone&pretty=1")
@@ -13,10 +14,11 @@ class SlackApi < ApplicationRecord
     request["Postman-Token"] = ENV['POSTMAN_TOKEN']
 
     @response = http.request(request)
+    @response = JSON.parse(@response)
   end
 
   def msg_search
-    @response.read_body
+    @response
   end
 
 end
