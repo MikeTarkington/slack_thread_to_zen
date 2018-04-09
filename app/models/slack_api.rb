6b1cb0ee192b098cb8b1cb4e9abe3875499fc7dc
Mike Tarkington <tarkmike@gmail.com>
2018-04-08 19:54:31 -0700

    Debugging initialization of SlackApi call for initial testing.

--- app/models/slack_api.rb
+++ app/models/slack_api.rb
@@ -5,7 +5,7 @@ class SlackApi < ApplicationRecord
 
   def initialize
     p '*' * 80
-    p url = ("https://slack.com/api/search.messages?token=#{ENV['SLACK_TOKEN']}&query=everyone&pretty=1")
+    p url = URI.encode_www_form("https://slack.com/api/search.messages?token=#{ENV['SLACK_TOKEN']}&query=everyone&pretty=1")
 
     p http = Net::HTTP.new(url.host, url.port)
 
@@ -22,3 +22,16 @@ class SlackApi < ApplicationRecord
   end
 
 end
+
+
+# p params =  {
+#             endpoint: 'https://slack.com/api/search.messages',
+#             slack_token: 'xoxp-300848166629-300079888624-301797961287-babcd37e66002c8202cd96bc301f0e16',
+#             query: 'everyone',
+#             pretty: '1'
+#           }
+# p uri = URI.encode_www_form(params)
+#
+# p response = Net::HTTP::Get.new(uri)
+# p response.read_body
+# # p uri.query = URI.encode_www_form(params)
