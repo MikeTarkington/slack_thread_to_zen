1d17703894430db8c80336e4929f300b0707e178
Mike Tarkington <tarkmike@gmail.com>
2018-04-29 17:18:06 -0700

    Due to risk of malformed messages being rejected by the api, use the breadcrumb hyperlink found in the search and prepend that to the final str sent to zend.

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -7,7 +7,9 @@ class MessageController < ApplicationController
     timestamp = slack.parse_timestamp(msg)
     thread_messages = slack.parse_chan_hist(chan_hist_resp, timestamp)
     thread_msgs_content = slack.thread_msgs_content(thread_messages)
+    breadcrumb_permalink = msg['messages']['matches'][0]['permalink']
     comment_str = slack.format_comment(thread_msgs_content)
+    comment_str.prepend("#{breadcrumb_permalink}\n")
     ZdApi.new(comment_str, params['text'])
   end
 
